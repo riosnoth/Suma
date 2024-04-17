a4dd10bd85e5ce8b9018ce624cd6fe51285b31c6
riosnoth <167269560+riosnoth@users.noreply.github.com>
2024-04-16 23:33:10 -0400

    Update suma.py

--- suma.py
+++ suma.py
@@ -1,18 +1,14 @@
-matriz = [[0, 0, 0],
-          [0, 0, 0],
-          [0, 0, 0]]
-
+matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for i in range(3):
   for j in range(3):
     matriz[i][j] = int(input(f"Ingrese el valor para la fila {i+1}, columna {j+1}: "))
+
 suma = 0
 for i in range(3):
   for j in range(3):
     suma += matriz[i][j]
 
-print("\nMatriz:")
+print("Matriz:")
 for i in range(3):
-  for j in range(3):
-    print(f"{matriz[i][j]:2}", end=" ")
-  print()
-print(f"\nSumatoria de los elementos: {suma}")
+  print(matriz[i])
+print(f"Suma total: {suma}")
